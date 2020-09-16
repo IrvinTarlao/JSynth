d38a56515a771bff9bc11dcbbc95dcdca8e50494
IrvinTarlao <irvin@millionvictories.com>
2020-09-16 16:32:07 +0200

    adding useEffect in Keyframes

--- src/components/KeysFrame.js
+++ src/components/KeysFrame.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Key from './Key';
 import './key.css';
 
@@ -18,6 +18,30 @@ const keysList = [
 ]
 
 const KeysFrame = () => {
+    const [pressedKeys, setPressedKeys] = useState([]);
+    const ALLOWED_KEYS = ['f', 'g', 'h', 'j', 'k', 'l', 'm', 't', 'y', 'u', 'o', 'p'];
+
+    useEffect(() => {
+        const onKeyDown = ({ key }) => {
+            if (ALLOWED_KEYS.includes(key) && !pressedKeys.includes(key)) {
+                setPressedKeys(previousPressedKeys => [...previousPressedKeys, key]);
+            }
+        }
+
+        const onKeyUp = ({ key }) => {
+            if (ALLOWED_KEYS.includes(key)) {
+                setPressedKeys(previousPressedKeys => previousPressedKeys.filter(k => k !== key));
+            }
+        }
+
+        document.addEventListener('keydown', onKeyDown);
+        document.addEventListener('keyup', onKeyUp)
+
+        return () => {
+            document.removeEventListener('keydown', onKeyDown);
+            document.removeEventListener('keyup', onKeyUp);
+        }
+    });
 
     return (
         <div style={{ width: '70%', height: '100%', display: 'flex', flexDirection: 'column', backgroundColor: '#171419', padding: '5px', color: 'white' }}>
